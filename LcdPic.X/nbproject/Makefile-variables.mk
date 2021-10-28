1553f4aa549232eb2baf9c97cd758a17ebce862f
AgentD1 <jacobandian3@gmail.com>
2021-10-28 10:18:58 -0400

    Renamed Project

---
+++ LcdPic.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,20 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# free configuration
+CND_ARTIFACT_DIR_free=dist/free/production
+CND_ARTIFACT_NAME_free=LcdPic.X.production.hex
+CND_ARTIFACT_PATH_free=dist/free/production/LcdPic.X.production.hex
+CND_PACKAGE_DIR_free=${CND_DISTDIR}/free/package
+CND_PACKAGE_NAME_free=lcdpic.x.tar
+CND_PACKAGE_PATH_free=${CND_DISTDIR}/free/package/lcdpic.x.tar
+# pro configuration
+CND_ARTIFACT_DIR_pro=dist/pro/production
+CND_ARTIFACT_NAME_pro=LcdPic.X.production.hex
+CND_ARTIFACT_PATH_pro=dist/pro/production/LcdPic.X.production.hex
+CND_PACKAGE_DIR_pro=${CND_DISTDIR}/pro/package
+CND_PACKAGE_NAME_pro=lcdpic.x.tar
+CND_PACKAGE_PATH_pro=${CND_DISTDIR}/pro/package/lcdpic.x.tar
